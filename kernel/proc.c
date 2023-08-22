25899754b1c46455fc6b27be5e45672661bea084
Yuanda Dong <dongyuanda@gmail.com>
2023-08-22 16:26:40 +1000

    what the fuck is happending?

--- kernel/proc.c
+++ kernel/proc.c
@@ -118,6 +118,14 @@ found:
         return 0;
     }
 
+    // Allocate a physical page to store process information such as pid
+    if ((p->userinfo = (struct usyscall *)kalloc()) == 0) {
+        freeproc(p);
+        release(&p->lock);
+        return 0;
+    }
+    p->userinfo->pid = p->pid;
+
     // An empty user page table.
     p->pagetable = proc_pagetable(p);
     if (p->pagetable == 0) {
@@ -142,6 +150,9 @@ static void freeproc(struct proc *p) {
     if (p->trapframe)
         kfree((void *)p->trapframe);
     p->trapframe = 0;
+    if (p->userinfo)
+        kfree((void *)p->userinfo);
+    p->userinfo = 0;
     if (p->pagetable)
         proc_freepagetable(p->pagetable, p->sz);
     p->pagetable = 0;
@@ -184,6 +195,14 @@ pagetable_t proc_pagetable(struct proc *p) {
         return 0;
     }
 
+    // map the userinfo page as USYSCALL with read-only access
+    if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->userinfo),
+                 PTE_R) < 0) {
+        uvmunmap(pagetable, USYSCALL, 1, 0);
+        uvmfree(pagetable, 0);
+        return 0;
+    }
+
     return pagetable;
 }
 
@@ -192,6 +211,7 @@ pagetable_t proc_pagetable(struct proc *p) {
 void proc_freepagetable(pagetable_t pagetable, uint64 sz) {
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
     uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, USYSCALL, 1, 0);
     uvmfree(pagetable, sz);
 }
 
